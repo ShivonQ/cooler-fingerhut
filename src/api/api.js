2d97a57fc58b751cc2fa50f720893abaec28c529
malcolm.leehan <malcolm.leehan@gmail.com>
2020-02-26 16:34:02 -0600

    making the app produce less chatter in console

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,7 @@
-import products from '../mock-data/products.json'
+import products from "../mock-data/products.json";
 
-const getProduct = (id) => {
+const getProduct = id => {
+  return products.filter(product => product.id === id)[0];
+};
 
-  return products.filter(product => product.id == id)[0]
-
-}
-
-export default { getProduct }
+export default { getProduct };
