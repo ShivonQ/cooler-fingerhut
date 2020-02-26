42187f6495e6956424c8a47a80835c87206f51bc
sandy.valdez <sandy.valdez@bluestembrands.com>
2020-02-26 11:21:26 -0600

    basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Form, FormControl, InputGroup, Nav, Navbar } from 'react-bootstrap'
+
+const Home = () => {
+
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Icon</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+          <Form inline>
+          <InputGroup className="mr-sm-2">
+            <FormControl
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faSearch} /></InputGroup.Text>
+            </InputGroup.Prepend>
+          </InputGroup>
+          </Form>
+          <Nav.Link href="#home">Login</Nav.Link>
+          <Nav.Link href="#home">Cart</Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>)
+
+}
+
+export default Home
+
