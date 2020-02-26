8b8536abc3cdd6e7a110bdf9549559d9fb086062
Sean.Zilka <sean.zilka@bluestembrands.com>
2020-02-26 15:14:55 -0600

    cart changes

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 import './Cart.css'
 const Cart = ({ cart }) => {
 
-  if (!cart.length) {
-    return (
-      <div className="container">
-        <div className="cart-container">
-          <div>Your cart is empty</div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className="container">
       <div className="cart-container">
