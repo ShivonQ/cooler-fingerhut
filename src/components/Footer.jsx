5adbec646a30273d48fbf9a847ae1411814871f5
sandy.valdez <sandy.valdez@bluestembrands.com>
2020-02-26 15:36:22 -0600

    new footer with disclaimers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 
   return (
-    <footer className="bg-dark pt-5 pb-5">
+    <footer className="bg-dark pt-5">
       <div className="container text-light">
         <div className="row">
           <div className="col-sm-4 col-md-3">
@@ -64,10 +64,53 @@ const Footer = () => {
           </div>
 
           <div className="col-sm-6 col-md-3">
-
             <h4 className="text-light">Catalog Search</h4>
           </div>
         </div>
+      </div>
+      <div className="bg-primary text-light p-4">
+      <div className="container-fluid">
+        <p><small>We have audited this and our other affiliate websites to ensure their accessibility. We are committed to maintaining the accessibility of this website to ensure that persons with disabilities have full and equal enjoyment of the goods, services, facilities, privileges, advantages, and accommodations of our services through the site.  If you have any accessibility questions or problems, please contact us at 1-800-208-2500 or customerservice@fingerhut.com for assistance.</small></p>
+        <div className="disclaimers padding padding-size-lg">
+          <p><small>Fingerhut Credit Accounts are issued by WebBank, Member FDIC.</small></p>
+          <p><small>* <strong>Advertised Price Per Month:</strong> The advertised price per month is the estimated monthly
+            payment required to be made on your
+            WebBank/Fingerhut Advantage Credit Account for a single item order, or if at any time your account has
+            multiple items on it, then please see the payment chart for payment terms. The advertised price per month
+            will not apply.
+            <a
+              href="http://s7d5.scene7.com/s7viewers/html5/BasicZoomViewer.html?closeButton=1&amp;serverUrl=http://s7d5.scene7.com/is/image/&amp;config=Scene7SharedAssets/Universal%5FHTML5%5FZoom%5Flight&amp;contenturl=http://s7d5.scene7.com/skins/&amp;asset=bluestembrands/TCO%20WEB%20TERMS"
+              data-track-id="globalFooter_link" data-track-value="Full Cost of Ownership">See Full Cost of Ownership</a>.
+          </small></p>
+          <p><small><sup>◆</sup> Your account minimum payment amount is determined by the purchases and balances on your
+            WebBank/Fingerhut Credit Account. If you have a balance or are making a multiple item purchase, see the <a
+              href="http://s7d5.scene7.com/s7viewers/html5/BasicZoomViewer.html?closeButton=1&amp;serverUrl=http://s7d5.scene7.com/is/image/&amp;config=Scene7SharedAssets/Universal%5FHTML5%5FZoom%5Flight&amp;contenturl=http://s7d5.scene7.com/skins/&amp;asset=bluestembrands/TCO%20WEB%20TERMS">Existing
+              Balance Payment Chart</a>.</small></p>
+          <p><small><sup></sup> Your account minimum payment amount is determined by the purchases and balances on your
+            WebBank/Fingerhut Credit Account. The change in minimum payment amount is based on your account balance at
+            the time it is advertised, and includes an assumption of interest charges and SafeLine fees, as applicable.
+            The change in minimum payment amount does not include subsequent activity on your account such as any
+            purchases, including deferred payment purchases, backordered, unshipped, returned or other pending purchases
+            or exchanges; any payments, including partial, missed or returned payments; any fees, including recurring
+            partner program charges, subsequent interest charges, late fees or returned payment fees. It is also subject
+            to change based on shipping address, shipping method, partial shipments and taxes. Your minimum payment
+            amount will not change and the purchase will not reflect on your statement until the order ships. If you
+            have a balance or are making a multiple item purchase, see the <a
+              href="http://s7d5.scene7.com/s7viewers/html5/BasicZoomViewer.html?closeButton=1&amp;serverUrl=http://s7d5.scene7.com/is/image/&amp;config=Scene7SharedAssets/Universal%5FHTML5%5FZoom%5Flight&amp;contenturl=http://s7d5.scene7.com/skins/&amp;asset=bluestembrands/TCO%20WEB%20TERMS">Existing
+              Balance Payment Chart</a>.</small></p>
+          <small>1. If you apply and are accepted for a Fingerhut Credit Account issued by WebBank, who determines
+            eligibility and qualifications for the terms of credit.</small>
+          <small>2. Please see our complete WebBank/Fingerhut Advantage Credit Account <a href="/content/footer-terms"
+                                                                                      data-track-id="globalFooter_link"
+                                                                                      data-track-value="PPM TCs">Terms
+            and Conditions</a>.</small>
+          <small className="visible-xs">Apple and the Apple logo are trademarks of Apple Inc., registered in the U.S. and
+            other countries. App Store is a service mark of Apple Inc. Google Play and the Google Play logo are
+            trademarks of Google Inc.</small>
+          <small className="copyright">© 2002-2020 Bluestem Brands, Inc. Fingerhut.com and designs are trademarks of
+            Bluestem Brands, Inc. All rights reserved.</small>
+        </div>
+      </div>
       </div>
     </footer>)
 
