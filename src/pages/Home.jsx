cffc327b334f0bcc21af91039d7c8102272d5eb8
sandy.valdez <sandy.valdez@bluestembrands.com>
2020-02-26 13:11:50 -0600

    adding images directly to project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,16 +13,16 @@ const Home = () => {
     <h2 className="d-inline mr-5"><a href="/PLP/5">ELECTRONICS</a></h2>
   </div>
     <div>
-      <img class="d-block w-100" src="https://files.slack.com/files-pri/T0AEY0UDP-FUJGLBC9K/image.png" alt="Womens" />
+      <img class="d-block w-100" src="/images/womens.png" alt="Womens" />
     </div>
     <div>
-      <img class="d-block w-100" src="https://files.slack.com/files-pri/T0AEY0UDP-FU86MDKS5/mens.png" alt="Mens" />
+      <img class="d-block w-100" src="/images/Mens.png" alt="Mens" />
     </div>
     <div>
       <img class="d-block w-100" src="https://s7d4.scene7.com/is/image/roomandboard/hartley_948499_18e1_g?hei=725&$str_g$" alt="Second slide" />
     </div>
     <div>
-      <img class="d-block w-100" src="https://files.slack.com/files-pri/T0AEY0UDP-FUJFX89PA/headphones.jpeg" alt="Third slide" />
+      <img class="d-block w-100" src="/images/Headphones.jpeg" alt="Third slide" />
     </div>
 </div>
 )
