ecf36ef6918b9477344bd7236c022d8b30e24280
malcolm.leehan <malcolm.leehan@gmail.com>
2020-02-26 16:39:46 -0600

    more cleanup ya'll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
-import './header.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { Form, FormControl, InputGroup, Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Form, FormControl, InputGroup, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = ({ cart }) => {
-
   return (
     <div>
       <div className="bg-dark pt-3 pb-1 fixed-top">
-        <p className="text-light text-center d-md-block d-sm-none d-none">free shipping on your order of $150 or more | promo code <strong>CMQ69</strong></p>
-        <p className="text-light text-center d-sm-block d-md-none">free shipping on your order of $150 or more <br/> promo code <strong>CMQ69</strong></p>
+        <p className="text-light text-center d-md-block d-sm-none d-none">
+          free shipping on your order of $150 or more | promo code{" "}
+          <strong>CMQ69</strong>
+        </p>
+        <p className="text-light text-center d-sm-block d-md-none">
+          free shipping on your order of $150 or more <br /> promo code{" "}
+          <strong>CMQ69</strong>
+        </p>
       </div>
       <Navbar className="pt-0 pb-0 nav-margin" bg="light" expand="lg">
-        <Navbar.Brand href="#home"><a href="/"><img src="/logo.png" width={75} alt="Cooler Fingerhut"/></a> </Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src="/logo.png" width={75} alt="Cooler Fingerhut" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-          </Nav>
+          <Nav className="mr-auto"></Nav>
           <Form inline>
-          <InputGroup className="mr-sm-2">
-            <FormControl
-              placeholder="Search Products"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-            <InputGroup.Prepend>
-              <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faSearch} /></InputGroup.Text>
-            </InputGroup.Prepend>
-          </InputGroup>
+            <InputGroup className="mr-sm-2">
+              <FormControl
+                placeholder="Search Products"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+            </InputGroup>
           </Form>
           <Nav.Link href="#home">Login</Nav.Link>
-          <Link to="/cart">Cart{(cart.length > 0 && <span>&nbsp;&#40;{cart.length}&#41;</span>)}</Link>
+          <Link to="/cart">
+            Cart{cart.length > 0 && <span>&nbsp;&#40;{cart.length}&#41;</span>}
+          </Link>
           <div className="d-lg-none">
             <Nav.Link href="/PLP/1">Sale</Nav.Link>
             <Nav.Link href="/PLP/2">Women</Nav.Link>
@@ -42,9 +52,8 @@ const Header = ({ cart }) => {
           </div>
         </Navbar.Collapse>
       </Navbar>
-    </div>)
+    </div>
+  );
+};
 
-}
-
-export default Header
-
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,70 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './pages/Home/Home';
-import PDP from './pages/PDP/PDP'
-import PLP from './pages/PLP/PLP'
-import Cart from './pages/Cart/Cart'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker';
-import "bootswatch/dist/lux/bootstrap.min.css"; 
-import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home/Home";
+import PDP from "./pages/PDP/PDP";
+import PLP from "./pages/PLP/PLP";
+import Cart from "./pages/Cart/Cart";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import "bootswatch/dist/lux/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import localStorageUtil from './util/localStorageUtil'
+import localStorageUtil from "./util/localStorageUtil";
 
 const App = () => {
+  const [cart, setCart] = useState(localStorageUtil.get("cart") || []);
 
-  const [cart, setCart] = useState(localStorageUtil.get('cart') || [])
+  const addToCart = item => {
+    const localCart = localStorageUtil.get("cart") || [];
 
-  const addToCart = (item) => {
-    const localCart = localStorageUtil.get('cart') || []
+    if (localCart.indexOf(item) < 0)
+      localStorageUtil.set("cart", [...cart, item]);
+    if (cart.indexOf(item) < 0) setCart([...cart, item]);
+  };
 
-    if (localCart.indexOf(item) < 0) localStorageUtil.set('cart', [...cart, item])
-    if (cart.indexOf(item) < 0) setCart([...cart, item])
-  }
-
-  const removeFromCart = (item) => {
-    localStorageUtil.set('cart', cart.filter(cartItem => cartItem.id != item.id))
-    setCart(cart.filter(cartItem => cartItem.id != item.id))
-  }
+  const removeFromCart = item => {
+    localStorageUtil.set(
+      "cart",
+      cart.filter(cartItem => cartItem.id !== item.id)
+    );
+    setCart(cart.filter(cartItem => cartItem.id !== item.id));
+  };
 
   return (
     <Router>
-  <div>
-    <Header cart={cart} />
-    <Route path={'/'} exact component={Home}/>
-    <Route exact path={`/PDP/:productId`} render={(props) => <PDP {...props} addToCart={addToCart} removeFromCart={removeFromCart} cart={cart} />} />
-    <Route component={PLP} exact path={`/PLP/:categoryId`} />
-    <Route exact path={`/cart`} render={(props) => <Cart {...props} removeFromCart={removeFromCart} cart={cart} />} />
-    <Footer/>
-  </div>
-  </Router>
-  )
+      <div>
+        <Header cart={cart} />
+        <Route path={"/"} exact component={Home} />
+        <Route
+          exact
+          path={`/PDP/:productId`}
+          render={props => (
+            <PDP
+              {...props}
+              addToCart={addToCart}
+              removeFromCart={removeFromCart}
+              cart={cart}
+            />
+          )}
+        />
+        <Route component={PLP} exact path={`/PLP/:categoryId`} />
+        <Route
+          exact
+          path={`/cart`}
+          render={props => (
+            <Cart {...props} removeFromCart={removeFromCart} cart={cart} />
+          )}
+        />
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
