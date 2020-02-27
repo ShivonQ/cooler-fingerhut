fdd96565dccb49f56afc5f667fb45cf1dbe579c0
sandy.valdez <sandy.valdez@bluestembrands.com>
2020-02-27 09:18:03 -0600

    plp mobile

--- src/pages/PLP/PLP.jsx
+++ src/pages/PLP/PLP.jsx
@@ -50,9 +50,9 @@ class PLP extends React.Component {
       const { name, title } = subCategory
       const selectedClass = name === selectedCategory ? 'selected' : ''
       return (
-        <div key={name} className="col-sm-3" onClick={(e) => this.handleOnClick(e, name)}>
+        <div key={name} className="col-6 col-lg-3 pb-3" onClick={(e) => this.handleOnClick(e, name)}>
           <div className={`card card-body sub-category-card border-dark justify-content-center ${selectedClass}`}>
-            <h4>{title}</h4>
+            <h4 className="m-0">{title}</h4>
           </div>
         </div>
       )
