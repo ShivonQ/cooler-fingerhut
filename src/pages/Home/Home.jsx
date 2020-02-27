40c4ed7c2dde70c5e6198af98b027113db84296a
sandy.valdez <sandy.valdez@bluestembrands.com>
2020-02-27 11:19:57 -0600

    adding hover effect to home page images.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,38 +26,46 @@ const Home = () => {
       <div className="row ml-1 mr-1 mb-4">
         <div className="col-md-3 p-0">
           <a href="/PLP/2">
+            <div className="overflow-hidden">
             <img
               className="image-container"
               src="https://asset1.cxnmarksandspencer.com/is/image/mands/SD_01_T49_1863_Y0_X_EC_1?$PDP_INT_IMAGEGRID_1_LG$"
               alt="Womens"
             />
+            </div>
           </a>
         </div>
         <div className="col-md-3 p-0">
           <a href="/PLP/3">
+            <div className="overflow-hidden">
             <img
               className="image-container"
               src="/images/Mens.png"
               alt="Mens"
             />
+            </div>
           </a>
         </div>
         <div className="col-md-3 p-0">
           <a href="/PLP/4">
+            <div className="overflow-hidden">
             <img
               className="image-container"
               src="https://s7d4.scene7.com/is/image/roomandboard/hartley_948499_18e1_g?hei=725&$str_g$"
               alt="Second slide"
             />
+            </div>
           </a>
         </div>
         <div className="col-md-3 p-0 mb-2">
           <a href="/PLP/5">
+            <div className="overflow-hidden">
             <img
               className="image-container"
               src="https://images.unsplash.com/photo-1545127398-14699f92334b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
               alt="Third slide"
             />
+            </div>
           </a>
         </div>
       </div>
